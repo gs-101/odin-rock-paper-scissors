39c8b98e11aaf8f330869bcd932404a71720fab7
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-04-27 10:52:30 -0300

    feat(flake.nix): add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Rock Paper Scissors Terminal Web game for The Odin Project.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+        inherit system;
+        pkgs = nixpkgs.legacyPackages.${system};
+      });
+
+    in {
+      devShells = forAllSystems ({ pkgs, ... }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            vscode-langservers-extracted
+          ];
+        };
+      });
+    };
+}
