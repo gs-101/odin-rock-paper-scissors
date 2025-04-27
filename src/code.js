69fac3084f81357a3b3cb6b1bc5e920d9be00442
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-04-27 11:38:24 -0300

    feat(code.js): add getComputerChoice()

--- src/code.js
+++ src/code.js
@@ -4,3 +4,20 @@ function getRandom(max) {
     return Math.floor(Math.random() * max);
 }
 
+/** Returns a pseudorandom string between "rock", "paper" or "scissors". This is
+ used to represent the adversary's choice in the game. */
+function getComputerChoice() {
+    let randomNumber = getRandom(3);
+
+    switch (randomNumber) {
+    // break; is not needed because the function ends after the return.
+    case 0:
+        return "rock";
+    case 1:
+        return "paper";
+    case 2:
+        return "scissors";
+    default:
+        throw new RangeError("Unexpected value, must be between 0 and 2.");
+    }
+}
